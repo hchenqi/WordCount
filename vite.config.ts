e8443f98e676601b5bd119670bf5b2c2b87d97ed
Chenqi <1002652154@qq.com>
2024-01-27 23:05:09 +0100

    initialize vite project

---
+++ vite.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        'name': 'Word Count',
+        'short_name': 'Word Count',
+        'start_url': '.',
+        'display': 'standalone',
+        'theme_color': '#ffdfbf',
+        'background_color': '#ffdfbf',
+        'icons': [
+          {
+            'sizes': '192x192',
+            'src': 'android-chrome-192x192.png',
+            'type': 'image/png'
+          },
+          {
+            'sizes': '512x512',
+            'src': 'android-chrome-512x512.png',
+            'type': 'image/png'
+          }
+        ]
+      },
+    })
+  ],
+  base: '',
+})
